b833f5006e32ebffe4c18527e852a3ce58422690
Andrew Bautista <andrewbautista85@gmail.com>
2026-06-23 15:29:31 -0400

    Enhance comments in initialize.js
    Add comments to clarify the purpose of the alias.

---
+++ aliases/initialize.js
@@ -0,0 +1,14 @@
+// This alias initializes variables into the userdata object for use with other alises and triggers.
+// It can be called manually, but it should be triggered on entrance into Genesis. (Check triggers)
+// Pattern: initialize
+
+// Execute the following javascript:
+  gwc.userdata.sap = true
+  gwc.userdata.katana = false
+  gwc.userdata.broadsword = false
+  gwc.userdata.tank = false
+  gwc.userdata.whetstone = false
+  gwc.userdata.dragonPresent = false
+  
+  gwc.output.append("Script initialized!")
+  gwc.output.color("lime")
