b8b7d5a0be6a0d565aaee239539ee7878f798859
Andrew Bautista <andrewbautista85@gmail.com>
2026-08-03 13:27:32 -0400

    Refactor stop_patrol.js for clarity and efficiency
    Updated comments for clarity and removed unused trigger disabling.

--- aliases/stop_patrol.js
+++ aliases/stop_patrol.js
@@ -1,15 +1,12 @@
-// This file essentially stops all movement and attack scripts used for patrolling.
-// For the sake of the sample, it only includes the throtyl pass triggers
+// This alias stops the patrol movement trigger from firing again after the next kill. Use <resume patrol> to restart the patrol in the same room, found in
+// resume_patrol.js
+//
 // However, you will want to include every single attack and movement trigger here that you create
 
 // Pattern: stop patrol
 
 // Execute the following javascript:
-// Disable attack triggers
-  gwc.trigger.disable('Util: Attack - Dwarf')
-
-// Disable movement triggers
-  gwc.trigger.disable('Util: Patrol - Throtyl Pass Movement')
+gwc.trigger.disable('Util: Patrol - Movement')
 
 // Disable misc. triggers
 // You can disable other triggers here as well. In this case, I have triggers for the strength spell and summoning dragons
