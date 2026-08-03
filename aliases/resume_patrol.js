f9a97033ec001617bed7f419f7653155a736d5a0
Andrew Bautista <andrewbautista85@gmail.com>
2026-08-03 13:48:33 -0400

    Update and rename resuming_patrol.js to resume_patrol.js

--- aliases/resume_patrol.js
+++ aliases/resume_patrol.js
@@ -6,22 +6,19 @@
 // For this example, it will only accept an argumment for throtyl pass, but you can accept more using a switch statement and by adding more cases.
 // Feel free to reach out if you need help coding this out to add more patrols.
 
-// Pattern: resuming 
-// An argument needs to be passed here. For this example, use <resume throtyl pass>
+// Pattern: resume patrol
 
 // Execute the following javascript:
-
-const patrol = args["*"].trim().toLowerCase();
-
-// Enables the kill and movement triggers based on the patrol, followed by the kill action to resume the script.
-  switch (patrol) {
-    case "throtyl pass":
-     gwc.trigger.enable('Util: Attack - Dwarf')
-     gwc.trigger.enable('Util: Patrol - Throtyl Pass Movement')
-
-     gwc.connection.send('k dwarf', true)
-     break; 
-    default:
-    // This will output an error message if you do not pass an acceptable patrol.
-    gwc.output.append("Invalid patrol. Try again.")
+  const lastTarget = gwc.userdata.patrol.lastTarget
+  const target = gwc.userdata.patrol.target
+  if (!lastTarget && !target) {
+    gwc.output.append("No target to set! Please set a target using <target>!")
+    return;
+  } else if (lastTarget && !target) {
+  gwc.connection.send(`target ${lastTarget}`, true)
+  return;
   }
+
+  gwc.trigger.enable('Util: Patrol - Movement')
+  
+  gwc.connection.send(`k ${target}`, true)
