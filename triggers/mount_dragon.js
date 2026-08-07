9cef92b66b3cd9f6180a9fffa31213e5e5eaea49
Andrew Bautista <andrewbautista85@gmail.com>
2026-08-07 11:35:11 -0400

    Add auto mount script for summoned dragon
    This script automatically mounts a summoned dragon using the specified command.

---
+++ triggers/mount_dragon.js
@@ -0,0 +1,10 @@
+/* Auto mount a dragon you summon using <dsteed>
+Note: This pattern uses the name of my dragon. Replace it with your dragon's name
+*/
+
+// Name: Util: Dragon - Mount
+// Type: plain
+// Pattern: Krythos, lands before you with a flurry of beating wings
+
+// Execute the following commands:
+  mount dragon
