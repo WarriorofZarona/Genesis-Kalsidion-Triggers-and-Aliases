cf3071db8db4bc2bc6715c74e0ca2a4cecc2bcdf
Andrew Bautista <andrewbautista85@gmail.com>
2026-08-07 13:08:51 -0400

    Enhance dsteed command to include passenger option
    Added support for a passenger argument in the dsteed command.

--- aliases/dsteed.js
+++ aliases/dsteed.js
@@ -4,6 +4,11 @@ You will need triggers to mount the dragon right away, which will also trigger y
 
 If no destination is used for <dsteed>, it will just summon the dragon like normal. 
 
+A second optional argument can be added in if you have a passenger. In which case, it will pull the passenger onto the mount with you.
+For example: <dsteed sanction kalsidion>
+
+Note: a destination must be inputted in order to take a passenger with you.
+
 Triggers needed:
 triggers/mount_dragon.js
 triggers/destination.js
@@ -18,3 +23,4 @@ triggers/dismount_dragon.js
 // Execute the following javascript:
   gwc.connection.send('!dsteed')
   gwc.userdata.destination = args[1]
+  gwc.userdata.passenger = args[2]
