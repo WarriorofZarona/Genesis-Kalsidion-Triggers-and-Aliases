2ce0a247ba4e9ada9692c0c18e8a9fb54e609011
Andrew Bautista <andrewbautista85@gmail.com>
2026-08-07 11:36:51 -0400

    Add auto dismount script for dragon landing
    This script automatically dismounts the player from their dragon upon landing at a destination, using a specific pattern to trigger the dismount command.

---
+++ triggers/dismount_dragon.js
@@ -0,0 +1,10 @@
+/* This auto dismounts you from your dragon once you land in your destination
+Note: This pattern uses the name of my dragon. Replace it with your dragon's name
+*/
+
+// Name: Util: Dragon - Dismount
+// Type: plain
+// Pattern: Krythos dives suddenly
+
+// Execute the following commands:
+  dismount
