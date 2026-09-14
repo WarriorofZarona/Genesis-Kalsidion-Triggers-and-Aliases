83b4bac8af7a75dca9a0b7205c4daf01a60cb8d7
Andrew Bautista <andrewbautista85@gmail.com>
2026-09-13 23:01:54 -0400

    Enhance pause_if_tired.js to shout when tired
    Added functionality to shout when tired if a leader is present.

--- triggers/pause_if_tired.js
+++ triggers/pause_if_tired.js
@@ -1,4 +1,5 @@
 // This file is used when your character runs out of stamina during a patrol. It calls the stop patrol script and also adjusts the index properly when you need to resume.
+// This is also useful if you are on patrol with someone else leading, and they leave you behind
 // Name it however you like
 
 // Name: Util: Patrol - Pause when tired
@@ -6,6 +7,14 @@
 // Pattern: You are too tired to move in that direction|is too tired to go there
 
 // Execute the following javascript:
+   const leader = mud.gmcp["char.team"].leader
+
+// When someone else is leading a hunt and you run out of stamina, shout.
+    if (leader && leader !== "You") {
+    gwc.connection.send("shout HOLD ON! I'm tired! Come back when you can!!")   
+      return
+    }
+  
   gwc.connection.send("wt Hold on... need to catch my breath.") 
   gwc.connection.send('stop patrol', true)
   const index = gwc.userdata.patrol.index - 1
